95df745e7acdfcf65d19f42ce4a07c113882e699
Gustavo <gustavogallesio@hotmail.com>
2024-05-27 11:19:43 -0300

    control de inicio de sesion

---
+++ HTML/js/validar_formulario.js
@@ -0,0 +1,17 @@
+function validarFormulario() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email.trim() === '' || password.trim() === '') {
+        alert('Por favor, completa todos los campos.');
+        return false;
+    }
+     // Validación de email
+     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+     if (!email.match(emailRegex)) {
+         alert('Por favor, ingresa una dirección de correo electrónico válida.');
+         return false;
+     }
+
+    return true;
+}
