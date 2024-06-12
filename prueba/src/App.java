22beac6bd1b8adff0f24827fb5717165e5fd9ea4
Gustavo Gallesio <gustavogallesio@hotmail.com>
2024-06-12 15:23:56 -0300

    java clase 1

---
+++ prueba/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int variable  = 10 , dato = 5;
+        /* int short long byte
+         * double float
+         * boolean
+         */
+        // comentario en una liena
+        
+        System.out.printf("%d %n",variable + dato);
+    }
+}
